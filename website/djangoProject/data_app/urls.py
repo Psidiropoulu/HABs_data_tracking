3b118a7e5453f47d5fe6fc9771892c8ece75d3b5
Psidiropoulu <157009506+Psidiropoulu@users.noreply.github.com>
2024-11-03 12:41:42 +0000

    /

--- website/djangoProject/data_app/urls.py
+++ website/djangoProject/data_app/urls.py
@@ -1,5 +1,6 @@
+# website/data_app/urls.py
 from django.urls import path
-from .views import LoRaWANDataView
+from .views import display_data, LoRaWANDataView  # Ensure both views are imported correctly
 
 urlpatterns = [
     path('api/data/', LoRaWANDataView.as_view(), name='lorawan-data'),
