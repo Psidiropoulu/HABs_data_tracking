89eea5178dfbc466c4b9857bca28ecc9d1b445ce
benjaminchang22 <benjamin.ty.chang@gmail.com>
2025-02-06 17:06:32 +0000

    Fix importing and timestamp data type

--- website/data_app/streamer/import_weather_data.py
+++ website/data_app/streamer/import_weather_data.py
@@ -3,15 +3,13 @@ import requests
 from data_app.models import WeatherData
 from datetime import datetime
 
-from data_app.models import WeatherData
-
 def import_data():
     # Replace with your logic to import data
     new_weather_data = WeatherData(
         temperature=25.0,  # Replace with actual data fields
         humidity=60.0,
         pressure=1013.0,
-        timestamp='2024-11-10 13:00:00'
+        timestamp = datetime.strptime('2024-11-10 13:00:00', '%Y-%m-%d %H:%M:%S'
     )
     new_weather_data.save()
     print("Weather data imported successfully.")
