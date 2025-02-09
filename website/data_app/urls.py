caa78b15b97de502bf83577a3e20688d67747875
Fiona <78430677+Fiona-F-ANG@users.noreply.github.com>
2025-02-09 11:59:41 +0000

    Resolved merge conflict in urls.py

--- website/data_app/urls.py
+++ website/data_app/urls.py
@@ -1,24 +1,12 @@
 # website/data_app/urls.py
+
 from django.urls import path
-<<<<<<< HEAD:website/djangoProject/data_app/urls.py
-from .views import weather_data_view
-from .views import weather_view
-from .views import skew_t_view, weather_data_view
-from .views import trajectory_view
-
-
+from .views import weather_data_view, submit_data_view, weather_view, skew_t_view, trajectory_view
 
 urlpatterns = [
     path('weather/', weather_data_view, name='weather_data'),
     path('live_weather/', weather_view, name='weather_view'),
     path('skew_t/', skew_t_view, name='skew_t_view'),
     path('trajectory/', trajectory_view, name='trajectory_view'),
+    path('api/addData', submit_data_view, name='submit_data'),  # Keeping both changes
 ]
-=======
-from .views import weather_data_view, submit_data_view
-
-urlpatterns = [
-    path('weather/', weather_data_view, name='weather_data'),
-    path('api/addData', submit_data_view, name='submit_data'),
-]
->>>>>>> origin/main:website/data_app/urls.py
