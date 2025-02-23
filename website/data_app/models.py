68c3e79bff7a7de8ffde9a93aa39fd18e02c0992
Psidiropoulu <157009506+Psidiropoulu@users.noreply.github.com>
2025-02-23 12:47:20 +0000

    added models to deal with the STATE file data

--- website/data_app/models.py
+++ website/data_app/models.py
@@ -4,9 +4,26 @@ from django.db import models
 class RealTimeData(models.Model):
     value = models.IntegerField()
     timestamp = models.DateTimeField(auto_now=True)
+    
+    #added new fields so that the data from the sensors can be saved into the RealTimeData (so, the file structure is the same as that of the STATE from main.h)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    altitude = models.FloatField(null=True, blank=True)
+    speed = models.FloatField(null=True, blank=True)
+    direction = models.FloatField(null=True, blank=True)
+    temperature = models.FloatField(null=True, blank=True)
+    pressure = models.FloatField(null=True, blank=True)
+    humidity = models.FloatField(null=True, blank=True)
+    no2we = models.FloatField(null=True, blank=True)
+    no2ae = models.FloatField(null=True, blank=True)
+    muon_count = models.IntegerField(null=True, blank=True)
+    muon_rate = models.FloatField(null=True, blank=True)
+    flight_mode = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.timestamp}: {self.latitude}, {self.longitude}, {self.altitude}"
 
 
-from django.db import models
 
 class WeatherData(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
--- website/trajectory_app/models.py
+++ website/trajectory_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Model to use the date from the STATE file, but only using the parameters that we need for our new model
+class TrajectoryData(models.Model):
+    timestamp = models.DateTimeField(auto_now=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    altitude = models.FloatField()
+    speed = models.FloatField(null=True, blank=True)
+    direction = models.FloatField(null=True, blank=True)
+    flight_mode = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return f"Trajectory: {self.timestamp}: {self.latitude}, {self.longitude}, {self.altitude}"
