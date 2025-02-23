5c4923df421c728eb3308b799883767b63e04cb7
Psidiropoulu <157009506+Psidiropoulu@users.noreply.github.com>
2025-02-23 13:14:04 +0000

    changing urls and templates to make the website work...

--- website/data_app/urls.py
+++ website/data_app/urls.py
@@ -2,6 +2,7 @@
 
 from django.urls import path
 from .views import weather_data_view, submit_data_view, weather_view, skew_t_view, trajectory_view, index
+from django.urls import path, include
 
 urlpatterns = [
     path('weather/', weather_data_view, name='weather_data'),
@@ -9,5 +10,6 @@ urlpatterns = [
     path('skew_t/', skew_t_view, name='skew_t_view'),
     path('trajectory/', trajectory_view, name='trajectory_view'),
     path('api/addData', submit_data_view, name='submit_data'),  # Keeping both changes
+    path('data/', include('data_app.urls')),
     path('', index, name='index'),
 ]
